2aba80995c918fd745f78470615c32a2ab12f32c
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2020-06-24 02:03:47 -0300

    Implementação do serviço de cadastro de carros

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rest-sd-2020-3
+
+## Web Services
+- Serviços que utilizam o protocolo HTTP na Web para realizar a execução de métodos remotos
+  - SOAP (XML)
+  - REST
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const porta = process.env.PORT || 2000;
+
+var listaDeCarros = [];
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get("/consulta/:pos", (req, res) => {
+    const posicao = req.params.pos;
+
+    res.send(JSON.stringify(listaDeCarros[posicao]));
+});
+
+app.get("/consulta", (req, res) => {
+    res.send(JSON.stringify(listaDeCarros));
+});
+
+app.post("/carro", (req, res) => {
+    const carro = req.body;
+
+    listaDeCarros.push(carro);
+
+    console.log(req.body);
+
+    res.send("OK");
+});
+
+app.listen(porta, () => {
+    console.log("Servidor inicializado!");
+});
